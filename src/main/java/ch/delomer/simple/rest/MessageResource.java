123912160a3601c2ca53d25c0dc4b7b1220fc2df
Remo Ledermann <rem.led@bluewin.ch>
2018-05-14 21:21:16 +0200

    inital commit

---
+++ src/main/java/ch/delomer/simple/rest/MessageResource.java
@@ -0,0 +1,12 @@
+package ch.delomer.simple.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("message")
+public class MessageResource {
+    @GET
+    public String whatever() {
+        return "hey, duke!";
+    }
+}
